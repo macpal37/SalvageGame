339c2736f6e14ef1de5075848946e3e76a4bf620
Iris Li <icl5@cornell.edu>
2022-03-14 21:23:54 -0400

    copied some files from physics lab, need to rename

--- core/src/com/xstudios/salvage/game/GDXRoot.java
+++ core/src/com/xstudios/salvage/game/GDXRoot.java
@@ -1,31 +1,90 @@
 package com.xstudios.salvage.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.xstudios.salvage.assets.AssetDirectory;
+import com.xstudios.salvage.util.ScreenListener;
 
-public class GDXRoot extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class GDXRoot extends Game implements ScreenListener {
+	/** AssetManager to load game assets (textures, sounds, etc.) */
+	AssetDirectory directory;
+	/** Drawing context to display graphics (VIEW CLASS) */
+	private GameCanvas canvas;
+	/** Player mode for the asset loading screen (CONTROLLER CLASS) */
+	private LoadingMode loading;
+	/** Player mode for the the game proper (CONTROLLER CLASS) */
+	private int current;
+	/** List of all WorldControllers */
+	private LevelController[] controllers;
+
+	/**
+	 * Called when the Application is first created.
+	 *
+	 * This is method immediately loads assets for the loading screen, and prepares
+	 * the asynchronous loader for all other assets.
+	 */
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		canvas  = new GameCanvas();
+		loading = new LoadingMode("assets.json",canvas,1);
+
+		// Initialize the three game worlds
+		controllers = new LevelController[3];
+//		controllers[0] = new TestLevelController();
+		current = 0;
+		loading.setScreenListener(this);
+		setScreen(loading);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 0, 1, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
-	
+
+	/**
+	 * Called when the Application is destroyed.
+	 *
+	 * This is preceded by a call to pause().
+	 */
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		// Call dispose on our children
+		setScreen(null);
+		for(int ii = 0; ii < controllers.length; ii++) {
+			controllers[ii].dispose();
+		}
+
+		canvas.dispose();
+		canvas = null;
+
+		// Unload all of the resources
+		if (directory != null) {
+			directory.unloadAssets();
+			directory.dispose();
+			directory = null;
+		}
+		super.dispose();
+	}
+
+	/**
+	 * Called when the Application is resized.
+	 *
+	 * This can happen at any point during a non-paused state but will never happen
+	 * before a call to create().
+	 *
+	 * @param width  The new width in pixels
+	 * @param height The new height in pixels
+	 */
+	public void resize(int width, int height) {
+		canvas.resize();
+		super.resize(width,height);
+	}
+
+	@Override
+	public void exitScreen(Screen screen, int exitCode) {
+
 	}
 }
