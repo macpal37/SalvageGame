a4dae5409506d913c4cad87ebae36460ca95f7ae
Emmi Wyttenbach <ezw2@cornell.edu>
2022-03-20 11:47:15 -0400

    try to fix movement and start item model

---
+++ core/src/com/xstudios/salvage/game/models/ItemModel.java
@@ -0,0 +1,206 @@
+package com.xstudios.salvage.game.models;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.JsonValue;
+import com.xstudios.salvage.game.GameCanvas;
+import com.xstudios.salvage.game.GameObject;
+
+public class ItemModel extends GameObject {
+
+    /** Shape information for this box */
+    protected PolygonShape shape;
+    /** A cache value for the fixture (for resizing) */
+    private Fixture geometry;
+    /** Cache of the polygon vertices (for resizing) */
+    private float[] vertices;
+    /** Type of item*/
+    private String item_type;
+    /** unique id of item*/
+    private int item_ID;
+    /** The factor to multiply by the input */
+    private final float force;
+    /** The amount to slow the character down */
+    private final float damping;
+    /** The maximum character speed */
+    private final float maxspeed;
+    /** The current horizontal movement of the item */
+    private float movement;
+
+
+
+    public ItemModel(JsonValue data, float width, float height, String item_type, int id){
+        super(data.get("pos").getFloat(0),
+                data.get("pos").getFloat(1));
+
+        shape = new PolygonShape();
+        origin = new Vector2();
+        body = null;
+        vertices = new float[8];
+        this.item_type = item_type;
+        this.item_ID = id;
+
+        setDensity(data.getFloat("density", 0));
+        setFriction(data.getFloat("friction", 0));  /// HE WILL STICK TO WALLS IF YOU FORGET
+        setMass(1);
+        setFixedRotation(true);
+        maxspeed = data.getFloat("maxspeed", 0);
+        damping = data.getFloat("damping", 0);
+        force = data.getFloat("force", 0);
+
+        // Initialize
+        resize(width/4, height/4);
+        resize(1, 1);
+        setMass(1);
+        resetMass();
+        setName(item_type);
+    }
+    /**
+     * Release the fixtures for this body, resetting the shape
+     *
+     * This is the primary method to override for custom physics objects
+     */
+    protected void releaseFixtures() {
+        if (geometry != null) {
+            body.destroyFixture(geometry);
+            geometry = null;
+        }
+    }
+    protected void createFixtures() {
+        if (body == null) {
+            return;
+        }
+
+        releaseFixtures();
+
+        // Create the fixture
+        fixture.shape = shape;
+        geometry = body.createFixture(fixture);
+        markDirty(false);
+    }
+
+    public boolean activatePhysics(World world) {
+
+        if (!super.activatePhysics(world)) {
+            return false;
+        }
+
+        // Make a body, if possible
+
+
+        bodyinfo.active = true;
+        body = world.createBody(bodyinfo);
+
+
+        body.setUserData(this);
+
+        body.setFixedRotation(false);
+        body.setType(BodyDef.BodyType.DynamicBody);
+        // Only initialize if a body was created.
+        if (body != null) {
+            createFixtures();
+            return true;
+        }
+//
+        bodyinfo.active = true;
+        return true;
+    }
+
+    /**
+     * Sets the object texture for drawing purposes.
+     *
+     * In order for drawing to work properly, you MUST set the drawScale.
+     * The drawScale converts the physics units to pixels.
+     *
+     * @param value  the object texture for drawing purposes.
+     */
+    public void setTexture(TextureRegion value) {
+        texture = value;
+        origin.set(texture.getRegionWidth()/2.0f, texture.getRegionHeight()/2.0f);
+    }
+
+
+    @Override
+    public void draw(GameCanvas canvas) {
+//        body.applyAngularImpulse(1f,false);
+        System.out.println("Mass: " + body.getMass());
+        if (texture != null) {
+
+            canvas.draw(texture, Color.WHITE,origin.x,origin.y,body.getPosition().x,body.getPosition().y,getAngle(),0.5f,0.5f);
+        }
+    }
+
+
+    @Override
+    public void drawDebug(GameCanvas canvas) {
+//        canvas.drawPhysics(shape,Color.GREEN,origin.x, origin.y);
+    }
+
+    /**
+     * Reset the polygon vertices in the shape to match the dimension.
+     */
+    private void resize(float width, float height) {
+        // Make the box with the center in the center
+        vertices[0] = -width/2.0f;
+        vertices[1] = -height/2.0f;
+        vertices[2] = -width/2.0f;
+        vertices[3] =  height/2.0f;
+        vertices[4] =  width/2.0f;
+        vertices[5] =  height/2.0f;
+        vertices[6] =  width/2.0f;
+        vertices[7] = -height/2.0f;
+        shape.setAsBox(width,height);
+    }
+
+    public void applyForce() {
+        if (!isActive()) {
+            return;
+        }
+
+        body.applyForce(new Vector2(getMovement()*10000,0),getPosition(),true);
+
+    }
+
+    /**
+     * Returns the x-coordinate for this physics body
+     *
+     * @return the x-coordinate for this physics body
+     */
+    public float getX() {
+        return (body != null ? body.getPosition().x : super.getX());
+    }
+
+    /**
+     * Returns the y-coordinate for this physics body
+     *
+     * @return the y-coordinate for this physics body
+     */
+    public float getY() {
+        return (body != null ? body.getPosition().y : super.getY());
+    }
+
+    public void setMovement(float value) {
+        movement = value;
+    }
+    /**
+     * Returns left/right movement of this character.
+     *
+     * This is the result of input times dude force.
+     *
+     * @return left/right movement of this character.
+     */
+    public float getMovement() {
+        return movement;
+    }
+
+
+    public String getItemType() {
+        return item_type;
+    }
+
+    public int getItemID() {
+        return item_ID;
+    }
+}
