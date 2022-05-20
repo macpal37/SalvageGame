e0b2f8c35ec612107515156b4cfd3bf4e0b26e4b
Asher Lipman <41AsherL@gmail.com>
2022-05-20 19:36:13 -0400

    Did a thingy
    please work

--- core/src/com/xstudios/salvage/game/GameController.java
+++ core/src/com/xstudios/salvage/game/GameController.java
@@ -180,7 +180,7 @@ public class GameController extends ScreenController implements ContactListener 
      */
     // Beta Release Setup
 //    private String[] levels = {"Golden0", "Golden1", "Golden2", "Golden3", "Golden4", "Golden5", "Golden6", "Golden7", "Golden8"};
-    private String[] levels = {"test_level", "Golden1", "Golden2", "Golden3", "Golden4", "Golden5", "Golden6", "Golden7", "Golden8"};
+    private String[] levels = {"Golden0", "Golden1", "Golden2", "Golden3", "Golden4", "Golden5", "Golden6", "Golden7", "Golden8", "Golden9", "Golden10", "Golden"};
 
 
     private int curr_level;
